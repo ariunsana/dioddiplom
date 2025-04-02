790dcc532bd4266b12aed16113ed5ff25596bd5b
Article <sw21d030@mandakh.edu.mn>
2025-04-02 13:59:12 +0800

    add models.py

--- backvollmedia/mediaapp/admin.py
+++ backvollmedia/mediaapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Team, Player, Game, PlayerStats, TeamStats
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(PlayerStats)
+admin.site.register(TeamStats)
